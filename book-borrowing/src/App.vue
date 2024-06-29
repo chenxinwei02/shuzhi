d3b3e3b6b6634da465aa7c77e5ec4e32c5878feb
chenxinwei02 <chenxinwei02@outlook.com>
2024-06-29 19:21:45 +0800

    前端

---
+++ book-borrowing/src/App.vue
@@ -0,0 +1,28 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app">
+    <BookBorrowing />
+  </div>
+</template>
+
+<script>
+import BookBorrowing from './components/BookBorrowing.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BookBorrowing
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
